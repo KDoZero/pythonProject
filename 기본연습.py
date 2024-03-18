f6b51bd219771b66847d62355aa60cc7cd8fa887
KDY <ehdudloro1018@gmail.com>
2024-03-18 17:40:59 +0900

    240318

--- 기본연습.py
+++ 기본연습.py
@@ -2,5 +2,33 @@ print("Hello, Python !!!")
 print(100)
 print(100 + 200)
 
-name = "김도영" #파이썬 데이터 타입이 없음, 반드시
+name = "김도영"  # 파이썬 데이터 타입이 없음
 print(name)
+
+# 파이썬의 주석입니다.
+name = "김도영"
+email = "ehdudloro1018@gmail.com"
+position = "학생"
+add = "경기도 수원시 영통동"
+print(f"""
+주소 : {add}
+직책 : {position}
+이름 : {name}
+이메일 : {email}
+""")
+
+if True:
+    print("정상")
+    print("입니다")
+else:
+    print("에러")
+
+'''
+작성자 : 김도영
+목적 : 파이썬 연습용 프로그램
+날짜 : 2024.03.18
+'''
+
+print(38) #숫자를 출력
+print("문자열 출력")
+print([1,2,3])
